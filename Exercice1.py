518fb06134d99c4e55944a70743e40cd6cdd2957
SambamD <demepoulo@gmail.com>
2022-02-04 22:01:53 +0100

    Power of

---
+++ Exercice1.py
@@ -0,0 +1,17 @@
+# 1 Python script to check if a given number is a power of another one
+from re import S
+import sys
+import math
+
+given = sys.argv[1]
+basee = sys.argv[2]
+
+def isPowerOf():
+    nmber = str(math.log(eval(given),eval(basee)))
+    u,v = nmber.split(".")
+    if v=='0':
+        print("Yes !")
+    else:
+        print("No !")
+        
+isPowerOf()
