38bb4a02c8a9d0b63fde9563523c7e5a64c20213
SambamD <demepoulo@gmail.com>
2022-02-05 03:14:10 +0100

    Find x numbers/sum(x) equals y

---
+++ Exercice3.py
@@ -0,0 +1,9 @@
+# Write a Python program to find x numbers from an array such that the sum of three numbers equal to y
+from itertools import combinations
+myArray = []
+def findXnumbers(myArray,x,y):
+    for i in combinations(myArray,x):
+        if(sum(i)==y):
+            print(i)
+
+findXnumbers([-1,2,-4,5,-2,3,1],3,1)
